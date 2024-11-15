d43e7b5b13c523b092a5d764b2bec8e2dc6074fd
WisnuNug <80598502+WisnuNug@users.noreply.github.com>
2024-11-15 22:10:37 +0700

    Update xnb-stardew.module.js

--- docs/js/libs/plugins/xnb-stardew.module.js
+++ docs/js/libs/plugins/xnb-stardew.module.js
@@ -5662,7 +5662,7 @@ var cropData = {
 	"IsPaddyCrop": "Boolean",
 	"NeedsWatering": "Boolean",
 	"$PlantableLocationRules": ["StardewValley.GameData.PlantableRule"],
-	"HarvestItemId": "Int32",
+	"HarvestItemId": "String",
 	"HarvestMinStack": "Int32",
 	"HarvestMaxStack": "Int32",
 	"HarvestMaxIncreasePerFarmingLevel": "Single",
