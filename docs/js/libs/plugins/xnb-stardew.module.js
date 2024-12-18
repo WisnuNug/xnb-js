31a9da3fbd592e5e1e4de4fd00ff7912caf246c0
WisnuNug <80598502+WisnuNug@users.noreply.github.com>
2024-12-18 10:33:32 +0700

    Update xnb-stardew.module.js

--- docs/js/libs/plugins/xnb-stardew.module.js
+++ docs/js/libs/plugins/xnb-stardew.module.js
@@ -5516,7 +5516,7 @@ var buildingSkin = {
 	$Condition: "String",
 	$BuildDays: "Int32",
 	$BuildCost: "Int32",
-	$BuildMaterials: ["StardewValley.GameData.Buildings.BuildingMaterial"],
+	BuildMaterials: ["StardewValley.GameData.Buildings.BuildingMaterial"],
 	ShowAsSeparateConstructionEntry: "Boolean",
 	$Metadata: {
 		"String": "String"
