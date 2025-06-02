430adc95c50271f55b7fa2c5c199edaaa8023b02
WisnuNug <80598502+WisnuNug@users.noreply.github.com>
2025-06-02 10:03:18 +0700

    Update xnb-stardew.module.js

--- docs/js/libs/plugins/xnb-stardew.module.js
+++ docs/js/libs/plugins/xnb-stardew.module.js
@@ -416,7 +416,6 @@ class Int32Reader extends BaseReader {
 	static isTypeOf(type) {
 		switch (type) {
 			case 'Microsoft.Xna.Framework.Content.Int32Reader':
-			case 'Microsoft.Xna.Framework.Content.EnumReader':
 			case 'System.Int32':
 				return true;
 			default:
@@ -431,7 +430,64 @@ class Int32Reader extends BaseReader {
 		buffer.writeInt32(content);
 	}
 }
+class EnumReader extends BaseReader {
 
+    static isTypeOf(type) {
+        switch (type) {
+            case 'Microsoft.Xna.Framework.Content.EnumReader':
+                return true;
+            default: return false;
+        }
+    }
+    constructor(name) {
+        super(); 
+        this.name = name;
+    }
+
+    isValueType() {
+        return true;
+    }
+
+    read(buffer) {
+        const intValue = buffer.readInt32();
+        const enumMap = enums[this.name];
+
+        if (enumMap && enumMap[intValue] !== undefined) {
+            return enumMap[intValue];
+        }
+
+        return intValue;
+    }
+
+    write(buffer, content, resolver) {
+        this.writeIndex(buffer, resolver);
+
+        const enumMap = enums[this.name];
+        let intValue = content;
+
+        if (enumMap && typeof content === 'string') {
+            const found = Object.entries(enumMap).find(([k, v]) => v === content);
+            if (found) {
+                intValue = parseInt(found[0]);
+            } else {
+                throw new Error(`Enum value "${content}" not found in ${this.name}`);
+            }
+        }
+
+        buffer.writeInt32(intValue);
+    }
+
+
+    toString() {
+        return `EnumReader<${this.name}>`;
+    }
+
+    get type() {
+        return `EnumReader<${this.name}>`;
+    }
+
+    
+}
 class ListReader extends BaseReader {
 	static isTypeOf(type) {
 		switch (type) {
